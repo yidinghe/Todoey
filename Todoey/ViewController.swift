abb93cf7c2693b430f638160b4a0b84ccaad5f4f
Yiding He <yidinghe2012@gmail.com>
2019-07-15 21:04:48 -0400

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Yiding He on 7/15/19.
+//  Copyright © 2019 Yiding He. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
