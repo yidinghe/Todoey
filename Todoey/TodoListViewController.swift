6f4bc5b4253c1c2c8b36bd00502093cf43200567
Yiding He <yidinghe2012@gmail.com>
2019-07-16 20:08:39 -0400

    Added select cell.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -24,9 +24,21 @@ class TodoListViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        
+
         cell.textLabel?.text = itemArray[indexPath.row]
-        
+
         return cell
+    }
+
+    // MARK: - Tableview Delegate Methods
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
