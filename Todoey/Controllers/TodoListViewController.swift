19bb3f990f9fd20f65bbe737ce08aa3b8b748142
Yiding He <yidinghe2012@gmail.com>
2019-07-17 20:56:46 -0400

    Added core data.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     var itemArray = [Item]()
 
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -53,9 +55,9 @@ class TodoListViewController: UITableViewController {
 
         let action = UIAlertAction(title: "Add Item", style: .default) { _ in
 
-            let newItem = Item()
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
-
+            newItem.done = false
             self.itemArray.append(newItem)
             self.tableView.reloadData()
             self.saveItems()
@@ -71,10 +73,8 @@ class TodoListViewController: UITableViewController {
     }
 
     func saveItems() {
-        let encoder = PropertyListEncoder()
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
             print("error: \(error)")
         }
@@ -82,13 +82,11 @@ class TodoListViewController: UITableViewController {
     }
 
     func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("error: \(error)")
-            }
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+           itemArray = try context.fetch(request)
+        } catch {
+            print("error: \(error)")
         }
     }
 }
