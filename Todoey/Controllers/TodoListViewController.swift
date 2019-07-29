8a29e80d00fa72fe33230dd8af1cfb1ad17ed899
Yiding He <yidinghe2012@gmail.com>
2019-07-29 19:33:10 -0400

    ContrastColor and gradient color.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Yiding He. All rights reserved.
 //
 
+import ChameleonFramework
 import RealmSwift
 import UIKit
 
@@ -25,6 +26,7 @@ class TodoListViewController: SwipeTableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.separatorStyle = .none
         loadItems()
     }
 
@@ -38,6 +40,12 @@ class TodoListViewController: SwipeTableViewController {
         let cell = super.tableView(tableView, cellForRowAt: indexPath)
         if let item = todoItems?[indexPath.row] {
             cell.textLabel?.text = item.title
+
+            if let color = UIColor(hexString: selectedCategory!.color)?.darken(byPercentage: CGFloat(indexPath.row) / CGFloat(todoItems!.count)) {
+                cell.backgroundColor = color
+                cell.textLabel?.textColor = ContrastColorOf(color, returnFlat: true)
+            }
+
             cell.accessoryType = item.done ? .checkmark : .none
         } else {
             cell.textLabel?.text = "No Items Added"
